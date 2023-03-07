ffc74d007ab049b244ea1846d9e330171eb679d3
Aslam Palakkal Ahammed <palakkalaslam@outlook.com>
2023-03-07 13:24:53 +0400

    Add files via upload

---
+++ forms/contact.php
@@ -0,0 +1,77 @@
+<?php
+  /**
+  * Requires the "PHP Email Form" library
+  * The "PHP Email Form" library is available only in the pro version of the template
+  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
+  * For more info and help: https://bootstrapmade.com/php-email-form/
+  */
+
+  // Replace contact@example.com with your real receiving email address
+  $receiving_email_address = 'palakkalaslam@gmail.com';
+
+  // if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+  //   include( $php_email_form );
+  // } else {
+  //   die( 'Unable to load the "PHP Email Form" Library!');
+  // }
+
+  // $contact = new PHP_Email_Form;
+  // $contact->ajax = true;
+  
+  // $contact->to = $receiving_email_address;
+  // $contact->from_name = $_POST['name'];
+  // $contact->from_email = $_POST['email'];
+  // $contact->subject = $_POST['subject'];
+
+  // // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  
+  // $contact->smtp = array(
+  //   'host' => 'smtp.hostinger.com',
+  //   'username' => 'noreply@palakkalaslam.com',
+  //   'password' => 'AmAslam@123',
+  //   'port' => '465'
+  // );
+  
+
+  // $contact->add_message( $_POST['name'], 'From');
+  // $contact->add_message( $_POST['email'], 'Email');
+  // $contact->add_message( $_POST['message'], 'Message', 10);
+  // // mail("someone@example.com","My subject",$msg);
+  
+
+ 
+  // $contact->recaptcha_secret_key = '6LfQF9UkAAAAAAhOLdi1mR9GEA3XgwvUoa5NmweA';
+//echo $contact->send();
+ 
+ 
+	$formok = true;
+
+	//form data
+
+	$name = ($_POST['name']);
+	$email = ($_POST['email']);
+	$message = ($_POST['message']);
+
+	//validation
+	if(empty($name) || empty($email) || empty($message)){
+		$formok = false;
+	}
+
+	if($formok){
+		$headers = "From: palakkalaslam@gmail.com" . "\r\n";
+		$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+		$emailbody = "<p>You have recieved a new message from the enquiries form on your website.</p>
+                  <p><strong>Name: </strong> {$name} </p>
+                  <p><strong>Email Address: </strong> {$email} </p>
+                  <p><strong>Message: </strong> {$message} </p> ";
+
+    mail("palakkalaslam@gmail.com","New Enquiry",$emailbody,$headers);
+
+   
+
+         
+
+}
+
+die( 'OK');
+?>
